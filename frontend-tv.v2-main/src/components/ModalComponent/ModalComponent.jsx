e854e7c468e95f853b4963d0cca7d143f0837928
PasculiTurpie <rodrigo.s.turpie@gmail.com>
2025-10-09 15:58:31 -0300

    Refactor IRD modal layout and improve responsiveness

--- frontend-tv.v2-main/src/components/ModalComponent/ModalComponent.jsx
+++ frontend-tv.v2-main/src/components/ModalComponent/ModalComponent.jsx
@@ -1,43 +1,44 @@
+import { useCallback, useId } from "react";
+
 import "./ModalComponent.css";
 import Close from "../../../public/images/close.svg";
 
+const ModalComponent = ({ modalOpen, title, setModalOpen, children }) => {
+    const titleId = useId();
 
-const ModalComponent = ({
-    modalOpen,
-    title,
-    setModalOpen,
-    children,
-}) => {
-    const handleCloseModal = () => {
+    const handleCloseModal = useCallback(() => {
         setModalOpen(false);
-    };
+    }, [setModalOpen]);
 
     return (
-        <>
-            <div
-                className={
-                    modalOpen
-                        ? "container__modal-component show__modal"
-                        : "container__modal-component close__modal"
-                }
-            >
-                <div className="form__modal-component">
+        <div
+            className={
+                modalOpen
+                    ? "container__modal-component show__modal"
+                    : "container__modal-component close__modal"
+            }
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby={titleId}
+        >
+            <div className="form__modal-component">
+                <div className="modal__header">
                     <div className="modal__title">
-                        <h4>{title}</h4>
+                        <h4 id={titleId}>{title}</h4>
                     </div>
-                    <img
+                    <button
+                        type="button"
                         className="modal__close"
-                        src={Close}
-                        alt="close"
                         onClick={handleCloseModal}
-                    />
-                <div className="form__modal-component-margen">
-                    {children}
+                        aria-label="Cerrar modal"
+                    >
+                        <img src={Close} alt="" aria-hidden="true" />
+                    </button>
+                </div>
 
-                </div>
-                </div>
+                <div className="form__modal-component-margen">{children}</div>
             </div>
-        </>
+        </div>
     );
 };
 
